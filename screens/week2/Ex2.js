19487ddcf4f16d69c660dd74c4967475f16a9f35
TopKung093 <86650720+TopKung093@users.noreply.github.com>
2021-11-25 17:56:27 +0700

    Week2

---
+++ screens/week2/Ex2.js
@@ -0,0 +1,17 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Ex2() {
+  return (
+    <View style={{
+        flex: 1,
+             flexDirection: 'row',
+             }}>
+             <View style={{
+        width: 100 , 
+        backgroundColor: 'powderblue'}} />
+        </View>
+        
+  );
+}
